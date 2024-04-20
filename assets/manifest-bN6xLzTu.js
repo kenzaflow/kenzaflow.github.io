c56407dcfd55dce982f7112a2399379886bf62f8
kenzaflow <kenzaflow@users.noreply.github.com>
2024-04-20 02:50:56 +0000

    Upgrade via GitHub Actions

---
+++ assets/manifest-bN6xLzTu.js
@@ -0,0 +1,1 @@
+import{c as o}from"./createReactComponent-5HZ3eZXj.js";import"./index-h1QH8epO.js";var a=o("folder","IconFolder",[["path",{d:"M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2",key:"svg-0"}]]);const t={appId:"explorer",name:"Explorador",icon:a,soon:!0};export{t as default};
